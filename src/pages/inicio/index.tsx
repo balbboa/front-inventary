a9768d4dfc945bc2ecf3c5889e87ff7b18ee129b
balbboa <abmm_arthur@hotmail.com>
2023-01-19 12:30:37 -0300

    Feat: tela inicial

--- src/pages/inicio/index.tsx
+++ src/pages/inicio/index.tsx
@@ -1,65 +1,21 @@
 // Next
 import {
-  Box,
   Flex,
-  Stat,
-  StatLabel,
-  useColorModeValue,
 } from "@chakra-ui/react";
 import { GetServerSideProps } from "next";
-import Router from "next/router";
 // Cookies
 import { parseCookies } from "nookies";
-import { ReactNode } from "react";
 // Layout
 import { FaCartPlus } from "react-icons/fa";
 import { FaUniversity } from "react-icons/fa";
 import { FaHammer } from "react-icons/fa";
 import { FaLaptop } from "react-icons/fa";
-
 import Layout from "../../components/Layout";
-import { colors } from "../../utils/colors";
+import StatsCard from "../../components/StatsCard";
 
-interface StatsCardProps {
-  title: string;
-  icon: ReactNode;
-  path: string;
-}
 
 const Home = () => {
-  const StatsCard = (props: StatsCardProps) => {
-    const { title, icon, path } = props;
-    return (
-      <Stat
-        m={"5"}
-        width={"sm"}
-        cursor={"pointer"}
-        px={{ base: 1, md: 5 }}
-        py={"5"}
-        shadow={"2xl"}
-        bg={useColorModeValue("white", colors.grayWolf)}
-        rounded={"lg"}
-        onClick={() => Router.push(path)
-        }
-      >
-        <Flex justifyContent={"start"} alignItems={"center"}>
-          <Box
-            my={"auto"}
-            color={useColorModeValue("gray.800", "gray.200")}
-            alignContent={"center"}
-          >
-            {icon}
-          </Box>
-          <Box pl={{ base: 2, md: 4 }}>
-            <StatLabel fontSize={"md"} fontWeight={"medium"}>
-              {title}
-            </StatLabel>
-          </Box>
-        </Flex>
-      </Stat>
-    );
-  };
-
+  
   return (
     <Layout props={{ title: "Início" }}>
       Bem-vindo ao sistema,
